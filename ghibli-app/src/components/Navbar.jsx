a79892b777394161d43e77ae9dcce214b8b0ae3a
AlexandreRob <alexandre.robert974@hotmail.fr>
2025-06-11 14:51:41 +0400

    Create Navbar.jsx

---
+++ ghibli-app/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
+        <div className="nav-brand">
+          <NavLink to="/" className="nav-logo">
+            Studio Ghibli
+          </NavLink>
+        </div>
+        <div className="nav-links">
+          <NavLink 
+            to="/" 
+            className={({ isActive }) => 
+              `nav-link ${isActive ? 'active' : ''}`
+            }
+          >
+            Accueil
+          </NavLink>
+
+          <NavLink 
+            to="/favorites" 
+            className={({ isActive }) => 
+              `nav-link ${isActive ? 'active' : ''}`
+            }
+          >
+            Favoris
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+}
